77b72c723059207748b14ebaba7d15b6ab9e31c0
Moncho Varela <nakome@gmail.com>
2024-03-19 22:59:54 +0100

    Remove old files

--- assets/js/modules/generateRandomId.js
+++
@@ -1,8 +0,0 @@
-/**
- * Generates a random ID.
- *
- * @return {string} a random ID
- */
-export default function generateRandomId() {
-    return Math.random().toString(36).substr(2, 9);
-}
--- assets/js/modules/isMobile.js
+++
@@ -1,8 +0,0 @@
-/**
- * Check if the user agent indicates that the device is a mobile device.
- *
- * @return {boolean} true if the device is a mobile device, false otherwise
- */
-export default function isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
