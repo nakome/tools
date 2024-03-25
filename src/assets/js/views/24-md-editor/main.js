a25aa127b784332f289682d18bfa99bf44a1da43
Moncho Varela <nakome@gmail.com>
2024-03-25 21:01:45 +0100

    Hide code on preview in mobile

--- src/assets/js/views/24-md-editor/main.js
+++ src/assets/js/views/24-md-editor/main.js
@@ -4,7 +4,7 @@ import sendPostMessage from "./controllers/sendPostMessage.js";
 import toggleFullPreview from "./controllers/toggleFullPreview.js";
 import toggleTheme from "./controllers/toggleTheme.js";
 
-import {Editor,preloader} from  "./controllers/vars.js";
+import {Editor,toggleView,horizontalSplitView,eyeSlashIcon} from  "./controllers/vars.js";
 
 if (storage("editor_md")) Editor.setValue(storage("editor_md"));
 
@@ -12,7 +12,18 @@ document.body.addEventListener("keydown", evt => (evt.ctrlKey && evt.keyCode ===
 
 saveHtmlCode.addEventListener("click", evt => exportToMd('filename.md', Editor.getValue()));
 
-renderCode.addEventListener("click", evt => sendPostMessage(evt));
+renderCode.addEventListener("click", evt => {
+    renderCode.addEventListener("click", evt => {
+        evt.preventDefault();
+        sendPostMessage();
+        // Hide code on click for mobile
+        if (navigator.userAgent.toLowerCase().match(/mobile/i)) {
+          toggleView.classList.add('active');
+          toggleView.innerHTML = eyeSlashIcon;
+          horizontalSplitView.setSizes([0, 100]);
+        }
+      });
+});
 
 toggleView.addEventListener("click", evt => toggleFullPreview());
 
