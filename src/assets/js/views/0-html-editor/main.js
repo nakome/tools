e9f9b540d27982cfa46b1c1968f032e59a03f69a
Moncho Varela <nakome@gmail.com>
2024-03-25 20:30:42 +0100

    Hide code on preview in mobile

--- src/assets/js/views/0-html-editor/main.js
+++ src/assets/js/views/0-html-editor/main.js
@@ -4,22 +4,22 @@ import {
   createNewSnippet,
   renderCode,
   settingsNew,
-  preloader,
+  horizontalSplitView,
+  eyeSlashIcon,
   toggleView,
   selectTheme,
   chooseLanguageHtml
 } from "./controllers/vars.js";
 import toggleFullPreview from "../../modules/toggleFullPreview.js";
 import storage from "../../modules/storage.js";
+import sendPostMessage from "../../modules/sendPostMessage.js";
 
 import handleToggleBlock from "./controllers/handleToggleBlock.js";
 import handlePostMessage from "./controllers/handlePostMessage.js";
 import handleOpenSettings from "./controllers/handleOpenSettings.js";
 import handleFormatCode from "./controllers/handleFormatCode.js";
-import handlePreviewCode from "./controllers/handlePreviewCode.js";
 import handleCreateNew from "./controllers/handleCreateNew.js";
 import handleSaveToFile from "./controllers/handleSaveToFile.js";
-import handleToggleFullPreview from "./controllers/handleToggleFullPreview.js";
 
 // Add event listeners to the buttons
 [...document.querySelectorAll("[id^='split-v-']")].forEach((e, i) => {
@@ -29,22 +29,34 @@ import handleToggleFullPreview from "./controllers/handleToggleFullPreview.js";
 });
 
 // Set the initial values
-if(storage("editor_html_type")) chooseLanguageHtml.value = storage("editor_html_type");
-if(storage("editor_html")) editors[0].setValue(storage("editor_html"));
-if(storage("editor_html")) editors[0].setValue(storage("editor_html"));
-if(storage("editor_css")) editors[1].setValue(storage("editor_css"));
-if(storage("editor_js")) editors[2].setValue(storage("editor_js"));
+if (storage("editor_html_type")) chooseLanguageHtml.value = storage("editor_html_type");
+if (storage("editor_html")) editors[0].setValue(storage("editor_html"));
+if (storage("editor_html")) editors[0].setValue(storage("editor_html"));
+if (storage("editor_css")) editors[1].setValue(storage("editor_css"));
+if (storage("editor_js")) editors[2].setValue(storage("editor_js"));
 
 document.body.addEventListener("keydown", handlePostMessage);
 saveHtmlCode.addEventListener("click", handleSaveToFile);
 createNewSnippet.addEventListener("click", handleCreateNew);
-renderCode.addEventListener("click", handlePreviewCode);
 settingsNew.addEventListener("click", handleOpenSettings);
-toggleView.addEventListener("click", handleToggleFullPreview);
+toggleView.addEventListener("click", toggleFullPreview);
+
+renderCode.addEventListener("click", evt => {
+  evt.preventDefault();
+  sendPostMessage();
+  // Hide code on click for mobile
+  if (navigator.userAgent.toLowerCase().match(/mobile/i)) {
+    console.log('view code')
+    toggleView.classList.add('active');
+    toggleView.innerHTML = eyeSlashIcon;
+    horizontalSplitView.setSizes([0, 100]);
+  }
+});
+
 selectTheme.addEventListener("change", evt => {
   let val = evt.currentTarget.value;
-  storage('editor_theme',val);
-  editors.forEach(editor => editor.setOption('theme',val));
+  storage('editor_theme', val);
+  editors.forEach(editor => editor.setOption('theme', val));
 })
 
 // If detect mobile device toggle the view
