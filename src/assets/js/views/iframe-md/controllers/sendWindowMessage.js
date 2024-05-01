187064237864dece3870ac4e42a24dd550f30ffe
Moncho Varela <nakome@gmail.com>
2024-05-01 14:22:59 +0200

    🚀 Add shiki for Markdown code

--- src/assets/js/views/iframe-md/controllers/sendWindowMessage.js
+++ src/assets/js/views/iframe-md/controllers/sendWindowMessage.js
@@ -1,11 +1,23 @@
+import { codeToHtml } from 'https://esm.sh/shiki@1.0.0'
 import decodeUnicode from "../../../modules/decodeUnicode.js";
+import sleep from "../../../modules/sleep.js";
 import {template} from "./vars.js";
 
 export default function sendWindowMessage(e) {
     let t = JSON.parse(e.data);
     document.body.innerHTML = template;
-    let i = setTimeout(() => {
+    sleep(1000).then(() => {
         document.body.innerHTML = decodeUnicode(t.body.content);
-        clearTimeout(i);
-    }, 1000);
+        sleep(200).then(() => {
+            const code = document.querySelectorAll('pre code[class^="language-"]');
+            code.forEach(async item => {
+                const html = item.textContent;
+                item.innerHTML = await codeToHtml(html, {
+                    lang: 'python',
+                    theme: 'dracula'
+                })
+            })
+        })
+    })
 }
+
